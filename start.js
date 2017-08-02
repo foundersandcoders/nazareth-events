e934bd3b526e3328abd524b7785ba6ee2713650f
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-02 10:28:59 +0300

    removed http createServer

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 const http = require('http');
 
-http.createServer(server).listen(server.get ('port'), function (){
-  console.log('Server listening on port ', server.get('port'));
-});
+server.listen(server.get('port'), function (){
+  console.log('Server listening on port', server.get('port'));
+})
