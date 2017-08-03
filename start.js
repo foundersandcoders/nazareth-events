058152fe019b4973a10ff5c597c7bbce822e21ea
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-03 10:47:29 +0300

    add semi colon, spaces for eslint

--- start.js
+++ start.js
@@ -1,6 +1,5 @@
 const server = require('./server.js');
-const http = require('http');
 
-server.listen(server.get('port'), function (){
+server.listen(server.get('port'), function () {
   console.log('Server listening on port', server.get('port'));
-})
+});
