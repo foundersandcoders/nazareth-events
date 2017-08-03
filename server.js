1954fbc9e5b435a74f5df692611c30f1c4cab625
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-03 11:27:22 +0300

    uncomment router

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ server.set('view engine', 'hbs');
 server.set('views', path.join(__dirname, './', 'views'));
 
 server.use(express.static(path.join(__dirname,'./','public')));
-// server.use(router);
+server.use(router);
 
-server.engine('hbs', hbs({
+server.engine('hbs', hbs ({
   defaultLayout: 'main',
-  layoutDir: path.join(__dirname, './','views/layouts'),
-  partialsDir: path.join(__dirname, './','views/partials'),
+  layoutDir: path.join(__dirname, './', 'views/layouts'),
+  partialsDir: path.join(__dirname, './', 'views/partials'),
   extname: 'hbs'
 }));
 
-module.exports = server
+module.exports = server;
