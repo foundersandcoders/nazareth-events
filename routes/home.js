fa36991f20bb9b70513b88023c7b16ef361ac589
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-07 11:21:10 +0300

    remove the api request from the server side

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,3 @@
-const request = require('request');
-
 module.exports = (req, res) => {
-    const baseURL = 'https://nazareth-open-tourism-platform.herokuapp.com' + '/events';
-    request(baseURL, function(error, response, body) {
-      const parseResult = JSON.parse(body);
-      const results = parseResult.filter(result => {
-
-      });
-      console.log(results);
-      res.render('home', {
-        events:parseResult
-      });
-    });
+  res.render('home', {title: 'events'});
 };
