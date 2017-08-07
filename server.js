6a405da12651ad4d630e9eb0b789c519e2c1b351
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-07 11:21:49 +0300

    made indentation

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ server.set('port', process.env.PORT || 8080);
 server.set('view engine', 'hbs');
 server.set('views', path.join(__dirname, './', 'views'));
 
-server.use(express.static(path.join(__dirname,'./','public')));
+server.use(express.static(path.join(__dirname, './', 'public')));
 server.use(router);
 
-server.engine('hbs', hbs ({
+server.engine('hbs', hbs({
   defaultLayout: 'main',
   layoutDir: path.join(__dirname, './', 'views/layouts'),
   partialsDir: path.join(__dirname, './', 'views/partials'),
