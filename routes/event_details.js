94855848cb5f8cd767546121b48ca3047c1cb5a7
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-13 14:44:37 +0300

    make another rquest for the place id with the information about the place

--- routes/event_details.js
+++ routes/event_details.js
@@ -6,12 +6,20 @@ module.exports = (req, res) => {
   request.get(url, (error, event) => {
     if (error) {
       res.send('error :(');
-    } else {
-      res.render('event_details', {
-        title: 'event details',
-        event: event,
-        back: req.headers.referer
-      });
     }
+    var placeId = JSON.parse(event.body).placeId;
+    var placeURL = 'https://nazareth-open-tourism-platform.herokuapp.com/places/' + placeId;
+    request.get(placeURL, (error, place) => {
+      var placeInfo = JSON.parse(place.body);
+      if (error) {
+        res.send('error :(');
+      } else {
+        res.render('event_details', {
+          title: 'event details',
+          placeInfo: placeInfo,
+          back: req.headers.referer
+        });
+      }
+    });
   });
 };
