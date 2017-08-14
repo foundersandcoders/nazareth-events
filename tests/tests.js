32b53019fc349e62f1a1e628dc42494fcb8046c0
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-14 15:47:31 +0300

    create folder tests inside it file tests.js

---
+++ tests/tests.js
@@ -0,0 +1,19 @@
+const tape = require('tape');
+const request = require('supertest');
+const server = require('../server.js');
+
+tape('test tape', (t) => {
+  t.ok(true, 'its working');
+  t.end();
+});
+
+tape('home route test: GET request to /', t => {
+  request(server)
+    .get('/')
+    .end((err, res) => {
+      if (err) throw err;
+      t.equal(res.status, 200, 'should return status code 200');
+      t.ok(res.text.includes('<div id="list-page-content">'), 'should return home page containing test string "<div id="list-page-content">"');
+      t.end();
+    });
+});
