50426db13dabba31030cac51f672ab37ac114906
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-23 12:02:05 +0300

    made router for the add-event page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homePage = require('./home.js');
 const detailsPage = require('./event_details.js');
+const addEventPage = require('./add-event.js');
 
 router.get('/', homePage);
 router.get('/events/:id', detailsPage);
+router.get('/add-event', addEventPage);
 
 module.exports = router;
