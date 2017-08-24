d75bb77c294ba40ec2f5d4e2e10819c1bc72392a
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-24 15:45:43 +0300

    didn't finshed it !

--- routes/add-event.js
+++ routes/add-event.js
@@ -1,3 +1,19 @@
+const request = require('request');
 module.exports = (req, res) => {
-  res.render('add-event', {title: 'add event'});
+  var url = 'https://nazareth-open-tourism-platform.herokuapp.com/events';
+
+  request.post(url, (error, result) => {
+    // var addEvent = { name: req.body.name, img: req.body.imageUrl, startTime: req.body.startTime, endTime: req.body.endTime, place: req.body.place, categories: req.body.categories, cost: req.body.cost, disabledaccess: req.body.accessibilityOptions, description: req.body.description };
+    // console.log(addEvent);
+    var event = JSON.parse(result.body);
+    console.log(event);
+
+    if (error) {
+      res.send('error :(');
+    } else {
+      res.render('add-event', {
+        title: 'add event'
+      });
+    }
+  });
 };
