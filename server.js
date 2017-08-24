ae9f41ea9f8b50ef4cfb411fedf5c810cdce4f9c
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-08-24 15:34:59 +0300

    use body-parser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const hbs = require('express-handlebars');
 const router = require('./routes/index.js');
 const dateFormat = require('dateformat');
+const bodyParser = require('body-parser');
 
 const server = express();
 
@@ -12,6 +13,8 @@ server.set('views', path.join(__dirname, './', 'views'));
 
 server.use(express.static(path.join(__dirname, './', 'public')));
 server.use(router);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 server.engine('hbs', hbs({
   defaultLayout: 'main',
