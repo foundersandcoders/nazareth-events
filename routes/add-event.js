26ae1d63647274872ba20afd9ae4f607061052c6
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-06 12:48:25 +0300

    req.body for the result from the ipnutes values

--- routes/add-event.js
+++ routes/add-event.js
@@ -2,18 +2,34 @@ const request = require('request');
 module.exports = (req, res) => {
   var url = 'https://nazareth-open-tourism-platform.herokuapp.com/events';
 
-  request.post(url, (error, result) => {
-    // var addEvent = { name: req.body.name, img: req.body.imageUrl, startTime: req.body.startTime, endTime: req.body.endTime, place: req.body.place, categories: req.body.categories, cost: req.body.cost, disabledaccess: req.body.accessibilityOptions, description: req.body.description };
-    // console.log(addEvent);
-    var event = JSON.parse(result.body);
-    console.log(event);
+  console.log('form:', req.body);
 
+  const requestBody = {
+    categories: req.body.categories,
+    accessibilityOptions: req.body.accessibilityOptions,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    cost: req.body.cost,
+    image: req.body.imageUrl,
+    en: {
+      name: req.body.name,
+      description: req.body.description
+    }
+  };
+  console.log(requestBody);
+
+  const options = {
+    method: 'post',
+    body: requestBody,
+    json: true,
+    url
+  };
+
+  request(options, (error, result, body) => {
     if (error) {
       res.send('error :(');
     } else {
-      res.render('add-event', {
-        title: 'add event'
-      });
+      res.send('you added event successfully');
     }
   });
 };
