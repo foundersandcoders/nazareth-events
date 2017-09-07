89e5b50e67d2926a577bd776b4d1027c49cc314d
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-07 10:42:55 +0300

    addEvent instead of addEventPage

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const homePage = require('./home.js');
 const detailsPage = require('./event_details.js');
-const addEventPage = require('./add-event.js');
+const addEvent = require('./add-event.js');
 
 router.get('/', homePage);
 router.get('/events/:id', detailsPage);
 router.get('/add-event', (req, res) => res.render('add-event', { title: 'add event' }));
-router.post('/add-event', addEventPage);
+router.post('/add-event', addEvent);
 
 module.exports = router;
