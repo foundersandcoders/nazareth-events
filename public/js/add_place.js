8af2935c05394de5411bc86eaa29d5e9a190829e
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-07 17:08:12 +0300

    dom stuff for search input

--- public/js/add_place.js
+++ public/js/add_place.js
@@ -1,6 +1,38 @@
 // add a new place
-document.getElementById('addPlaceBtn').addEventListener('click', function (e) {
+document.getElementById('addPlaceIcon').addEventListener('click', function (e) {
   document.getElementById('addPlaceContainer').classList.toggle('show');
   document.getElementById('selectAllPlaces').classList.toggle('hide');
   document.getElementById('searchPlace').classList.toggle('hide');
+});
+
+// search places
+var input = document.getElementById('searchPlace');
+input.addEventListener('keyup', function (e) {
+  var inputVal = input.value.trim().toUpperCase();
+  var list = document.querySelectorAll('.placesList');
+  var listOptions = [].slice.call(list).map(option => {
+    return option.textContent;
+  });
+
+  var copyOption = document.getElementById('option-list-container');
+  copyOption.innerHTML = '';
+
+  // create new ul
+  var resultsList = document.createElement('ul');
+  resultsList.id = 'word-list';
+
+  // create elements and add words
+  var arr = listOptions.filter(function (item) {
+    if (inputVal === '') {
+      return;
+    }
+    return item.startsWith(inputVal);
+  });
+  arr.forEach(function (item) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = item;
+    resultsList.appendChild(listItem);
+  });
+
+  copyOption.appendChild(resultsList);
 });
