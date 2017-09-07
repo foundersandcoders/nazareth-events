f70d818081e4edce9fe4908b4120a0286271495b
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-07 17:09:54 +0300

    make router for a add_places_get.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 const homePage = require('./home.js');
 const detailsPage = require('./event_details.js');
 const addEventPage = require('./add-event.js');
+const getPlaces = require('./add_places_get.js');
 
 router.get('/', homePage);
 router.get('/events/:id', detailsPage);
-router.get('/add-event', (req, res) => res.render('add-event', { title: 'add event' }));
 router.post('/add-event', addEventPage);
+router.get('/add-event', getPlaces);
 
 module.exports = router;
