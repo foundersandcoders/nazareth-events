79a11ee121e8b88d7b6660174bc5ab6cd29a8f8c
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-10 13:28:03 +0300

    add start date end date for the request with status code

--- routes/add-event.js
+++ routes/add-event.js
@@ -5,8 +5,8 @@ module.exports = (req, res) => {
   const requestBody = {
     categories: req.body.categories,
     accessibilityOptions: req.body.accessibilityOptions,
-    startTime: req.body.startTime,
-    endTime: req.body.endTime,
+    startTime: req.body.startDate + 'T' + req.body.startTime,
+    endTime: req.body.endDate + 'T' + req.body.endTime,
     cost: req.body.cost,
     image: req.body.imageUrl,
     en: {
@@ -14,19 +14,23 @@ module.exports = (req, res) => {
       description: req.body.description
     }
   };
-
   const options = {
     method: 'post',
     body: requestBody,
     json: true,
     url
   };
-
   request(options, (error, result, body) => {
     if (error) {
-      res.send('error :(');
+      res.send(error);
     } else {
-      res.send('you added event successfully');
+      if (result.statusCode >= 400 && result.statusCode <= 499) {
+        res.send(`error :( ${result.body.reasons}`);
+      } else if (result.statusCode >= 500 && result.statusCode <= 599) {
+        res.send('server error');
+      } else if (result.statusCode >= 200 && result.statusCode <= 299) {
+        res.redirect('/');
+      }
     }
   });
 };
