3b88b4cd6641432c8240405c039f45e3c2d9fd4c
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-10 16:51:32 +0300

    made getEvents function in object to use it on another file

--- public/js/get_events.js
+++ public/js/get_events.js
@@ -1,17 +1,22 @@
-/* global  XMLHttpRequest */
-function getEvents (path, callback) {
-  var baseUrl = 'https://nazareth-open-tourism-platform.herokuapp.com/events';
-  var url = baseUrl + path;
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(null, JSON.parse(xhr.responseText));
-    } else if (xhr.status === 500) {
-      var errorMessage = new Error('Server Error! Status: ' + xhr.status);
-      callback(errorMessage, null);
+/* global XMLHttpRequest */
+var module = (function () {
+  console.log(module);
+  return {
+    getEvents: function (path, callback) {
+      var baseUrl = 'https://nazareth-open-tourism-platform.herokuapp.com/events';
+      var url = baseUrl + path;
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          console.log('date from', xhr.responseText);
+          callback(null, JSON.parse(xhr.responseText));
+        } else if (xhr.status === 500) {
+          var errorMessage = new Error('Server Error! Status: ' + xhr.status);
+          callback(errorMessage, null);
+        }
+      };
+      xhr.open('GET', url, true);
+      xhr.send();
     }
   };
-  xhr.open('GET', url, true);
-  xhr.send();
-};
-module.exports = getEvents;
+})();
