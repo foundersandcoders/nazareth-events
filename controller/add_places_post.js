77602f070b5c1a85a72c698837c9cb25ec8e6797
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-14 16:10:02 +0300

    made folder controller get,pot places

---
+++ controller/add_places_post.js
@@ -0,0 +1,35 @@
+const request = require('request');
+module.exports = (req, res) => {
+  var id = req.params.id;
+  var url = 'https://nazareth-open-tourism-platform.herokuapp.com/places' + id;
+
+  const requestBody = {
+    location: req.body.place_location,
+    categories: req.body.place_categories,
+    accessibilityOptions: req.body.place_accessibilityOptions,
+    imageUrl: req.body.place_imageUrl,
+    website: req.body.place_website,
+    phone: req.body.place_phone,
+    email: req.body.place_email,
+    en: {
+      name: req.body.place_name,
+      address: req.body.place_address,
+      description: req.body.description,
+      openingHours: req.body.place_openingHours
+    }
+  };
+
+  const options = {
+    method: 'post',
+    json: true,
+    url
+  };
+
+  request(options, (error, result, body) => {
+    if (error) {
+      res.send('error');
+    } else {
+      console.log(requestBody);
+    }
+  });
+};
