f4ecd8fa98be64fd6efacacc1d192bc3c5e90d84
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-14 17:04:34 +0300

    added to module add_places_get.js directory folder ../controller

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 const homePage = require('./home.js');
 const detailsPage = require('./event_details.js');
-const getPlaces = require('./add_places_get.js');
+const getPlaces = require('../controller/add_places_get.js');
 const addEvent = require('./add-event.js');
 
 router.get('/', homePage);
 router.get('/events/:id', detailsPage);
-router.get('/add-event', (req, res) => res.render('add-event', { title: 'add event' }));
+router.get('/add-event', (req, res) => res.render('add-event', { title: 'add new event' }));
 router.get('/add-event', getPlaces);
 router.post('/add-event', addEvent);
 
