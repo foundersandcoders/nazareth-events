561e17762a25cb9f208c66b4755cf0e8c2b8ed30
Mario <mario.saliba98@gmail.com>
2017-09-16 18:41:44 +0300

    disables the unused vars rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   'extends': 'standard',
   rules: {
     semi: ['error', 'always'],
-    "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    "no-unused-vars": 0
   }
 }
