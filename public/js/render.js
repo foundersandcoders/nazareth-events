055d383c42363bae4931221caf1fe46fd7457531
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-09-25 15:13:16 +0300

    change time to 24 hours in home page

--- public/js/render.js
+++ public/js/render.js
@@ -31,7 +31,7 @@ var renderModule = function (error, eventsArray) {
     aElement.className = 'event-link';
 
     h3Element.innerHTML = event.en.name;
-    h4Element.innerHTML = new Date(event.startTime).toDateString() + '</br>' + new Date(new Date(event.startTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) + ' - ' + new Date(new Date(event.endTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    h4Element.innerHTML = new Date(event.startTime).toDateString() + '</br>' + new Date(new Date(event.startTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false}) + ' - ' + new Date(new Date(event.endTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false});
 
     aElement.appendChild(h3Element);
     aElement.appendChild(h4Element);
