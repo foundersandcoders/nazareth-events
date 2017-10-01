533acef64fa606b778f44d0a1025acbf20d5625d
Mario <mario.saliba98@gmail.com>
2017-10-01 12:46:27 +0300

    adds test to the middlewares

--- tests/tests.js
+++ tests/tests.js
@@ -3,6 +3,8 @@ const supertest = require('supertest');
 const server = require('../server.js');
 const request = require('request');
 
+const postPlace = require('../routes/middleware/post_place.js');
+
 tape('home route test: GET request to /', t => {
   supertest(server)
     .get('/')
@@ -15,11 +17,11 @@ tape('home route test: GET request to /', t => {
 });
 
 tape('Test the event details route', t => {
-  const htmlSample = '<h1 class="text-warning eventName">default</h1>';
+  const htmlSample = '<h1 class="text-warning eventName">FACN2</h1>';
   let eventId = '';
   request.get('https://nazareth-open-tourism-platform.herokuapp.com/events', (err, res) => {
     if (err) return err;
-    const filteredResult = JSON.parse(res.body).filter((event) => event.en.name === 'default');
+    const filteredResult = JSON.parse(res.body).filter((event) => event.en.name === 'FACN2');
     eventId = filteredResult[0]._id;
     supertest(server)
       .get('/events/' + eventId)
@@ -32,17 +34,45 @@ tape('Test the event details route', t => {
   });
 });
 
-tape('Tests what renders when you get an error in event details', t => {
-  const original = request.get;
-  request.get = (url, callback) => {
-    process.nextTick(callback, new Error('hushs'));
-  };
+// Work in progress
+// tape('Tests what renders when you get an error in event details', t => {
+//   const original = request.get;
+//   request.get = (url, callback) => {
+//     process.nextTick(callback, new Error('hushs'));
+//   };
+//   supertest(server)
+//     .get('/events/59afaf75b0a5e80011beeafa')
+//     .end((err, res) => {
+//       t.error(err);
+//       t.ok(res.text.includes();
+//       request.get = original;
+//       t.end();
+//     });
+// });
+
+tape('Test if it\'s rendering the add event form currectley', t => {
+  const htmlSample = '<h3 class="userAddEvent">add event</h3>';
   supertest(server)
-    .get('/events/59afaf75b0a5e80011beeafa')
+    .get('/add-event')
+    .expect(200)
     .end((err, res) => {
       t.error(err);
-      t.ok(res.text.includes('error :('));
-      request.get = original;
+      t.ok(res.text.includes(htmlSample));
+      t.end();
+    });
+});
+
+tape('Test if it posts the event to the api', t => {
+  const requestBody = {
+    name: 'random',
+    categories: ['music']
+  };
+  supertest(server)
+    .post('/add-event')
+    .send(requestBody)
+    .end((err, res) => {
+      t.error(err);
+      console.log(res.text);
       t.end();
     });
 });
