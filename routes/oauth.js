31984a4d7552aa24128439101b0f2f83c31c64fe
Mario <mario.saliba98@gmail.com>
2017-10-02 22:54:33 +0300

    adds a lifetime to the cookie

--- routes/oauth.js
+++ routes/oauth.js
@@ -7,18 +7,18 @@ module.exports = (req, res) => {
   if (req.query.state !== process.env.STATE) {
     res.send('Access not allowed');
   } else {
-    const tokenQueries = {
+    const tokenQueries = qs.stringify({
       code: req.query.code,
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
       redirect_uri: process.env.REDIRECT_URI,
       grant_type: 'authorization_code'
-    };
+    });
 
     const tokenRequestOptions = {
       method: 'POST',
       uri: 'https://nazareth-open-tourism-platform.herokuapp.com/oauth/token',
-      body: qs.stringify(tokenQueries),
+      body: tokenQueries,
       headers: {
         'content-type': 'application/x-www-form-urlencoded'
       }
@@ -29,9 +29,9 @@ module.exports = (req, res) => {
         return res.send('Something went wrong, please try again');
       } else {
         const parsedBody = JSON.parse(body);
-        const token = jwt.sign(parsedBody.access_token, 'jackisweird');
+        const token = jwt.sign(parsedBody.access_token, process.env.JWT_SECRET);
 
-        res.cookie('token', token);
+        res.cookie('token', token, { maxAge: 604800000 });
         res.redirect('/add-event');
       }
     });
