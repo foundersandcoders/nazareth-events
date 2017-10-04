8f6afcc849457a3d54215d546787a431482f2d1d
Mario <mario.saliba98@gmail.com>
2017-10-04 13:32:49 +0300

    use jwt to verify the token

--- routes/middleware/authenticate.js
+++ routes/middleware/authenticate.js
@@ -1,16 +1,24 @@
 const qs = require('querystring');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  if (req.cookies.token) {
-    return next();
-  } else {
-    const queries = qs.stringify({
-      client_id: process.env.CLIENT_ID,
-      redirect_uri: process.env.REDIRECT_URI,
-      state: process.env.STATE
-    });
+  const tokenCookie = req.cookies.token;
+  const queries = qs.stringify({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    state: process.env.STATE
+  });
 
+  if (tokenCookie) {
+    jwt.verify(tokenCookie, process.env.JWT_SECRET, (err) => {
+      if (err) {
+        res.redirect(`https://nazareth-open-tourism-platform.herokuapp.com/oauth/authorize?${queries}`);
+      } else {
+        return next();
+      }
+    });
+  } else {
     res.redirect(`https://nazareth-open-tourism-platform.herokuapp.com/oauth/authorize?${queries}`);
   }
 };
