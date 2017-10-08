5689c705090c370fa27e3adc8fc1beced69ba8ba
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-10-08 13:05:45 +0300

    added addEventListener to swich languages

--- public/js/render.js
+++ public/js/render.js
@@ -1,3 +1,5 @@
+/* global getEventsModule type:true */
+/* eslint no-global-assign: "error" */
 var renderModule = function (error, eventsArray) {
   var listPage = document.getElementById('list-page-content');
   var eventsSection = document.getElementById('events-section');
@@ -14,9 +16,33 @@ var renderModule = function (error, eventsArray) {
     eventsSection.innerHTML = error;
   }
 
-  var filterEvents = eventsArray.filter(function (event) {
-    return event.en;
+  var filterEvents = [];
+
+  document.getElementById('arLang').addEventListener('click', function () {
+    type = 'Arabic';
+    getEventsModule({
+      date_from: new Date().toISOString().slice(0, 10)
+    }, renderModule);
   });
+
+  document.getElementById('enLang').addEventListener('click', function () {
+    type = 'English';
+    getEventsModule({
+      date_from: new Date().toISOString().slice(0, 10)
+    }, renderModule);
+  });
+
+  switch (type) {
+    case 'Arabic':
+      filterEvents = eventsArray.filter(function (event) {
+        return event.ar;
+      });
+      break;
+    default:
+      filterEvents = eventsArray.filter(function (event) {
+        return event.en;
+      });
+  }
 
   filterEvents.forEach(function (event) {
     var eventContainerDiv = document.createElement('div');
@@ -30,7 +56,7 @@ var renderModule = function (error, eventsArray) {
     aElement.href = '/events/' + id;
     aElement.className = 'event-link';
 
-    h3Element.innerHTML = event.en.name;
+    h3Element.innerHTML = type === 'Arabic' ? event.ar.name : event.en.name;
     h4Element.innerHTML = new Date(event.startTime).toDateString() + '</br>' + new Date(new Date(event.startTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC'}) + ' - ' + new Date(new Date(event.endTime).getTime()).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC'}) + '<br>' + (event.place ? 'At ' + event.place.en.name : '');
 
     aElement.appendChild(h3Element);
