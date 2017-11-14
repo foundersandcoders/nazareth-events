defe215292b434ec9436dc796fc43bf09f367cc9
mario <mario.saliba98@gmail.com>
2017-11-14 19:54:08 +0200

    integrate google maps api into the app #87

--- routes/event_details.js
+++ routes/event_details.js
@@ -1,19 +1,43 @@
-const request = require('request');
+const axios = require('axios');
+require('dotenv').config();
 
-module.exports = (req, res) => {
-  const url = `https://nazareth-open-tourism-platform.herokuapp.com/api/v1/events/${req.params.id}`;
+module.exports = async (req, res) => {
+  const OTPUrl = `https://nazareth-open-tourism-platform.herokuapp.com/api/v1/events/${
+    req.params.id
+  }`;
 
-  request.get(url, (error, result) => {
-    if (error) {
-      res.send(error);
+  try {
+    const specificEventResponse = await axios.get(OTPUrl);
+    const event = specificEventResponse.data;
+    let lng;
+    let lat;
+    if (event.place.location) {
+      console.log(event.place.location);
+      lat = event.place.location[0];
+      lng = event.place.location[1];
     } else {
-      res.render('event_details', {
-        title: 'Event Details',
-        eventText: JSON.parse(result.body)[req.params.lang],
-        event: JSON.parse(result.body),
-        place: JSON.parse(result.body).place[req.params.lang],
-        back: req.headers.referer
-      });
+      let address = event.place[req.params.lang].address;
+      const geocode = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${
+          address
+        }&region=il&key=${process.env.GEOCODE_KEY}`
+      );
+      const { location } = geocode.data.results[0].geometry;
+      lng = location.lng;
+      lat = location.lat;
     }
-  });
+
+    res.render('event_details', {
+      title: 'Event Details',
+      eventText: event[req.params.lang],
+      event,
+      place: event.place[req.params.lang],
+      back: req.headers.referer,
+      key: process.env.GOOGLE_MAPS_KEY,
+      lng,
+      lat
+    });
+  } catch (err) {
+    console.log('me', err);
+  }
 };
