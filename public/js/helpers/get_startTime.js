63d59e448e2be5be795a9c591f71533b0328b6ee
mario <mario.saliba98@gmail.com>
2017-11-17 13:48:49 +0200

    add a api request to get event and their startTime

---
+++ public/js/helpers/get_startTime.js
@@ -0,0 +1,16 @@
+var dateArray = function (callback) {
+  axios
+    .get('https://nazareth-open-tourism-platform.herokuapp.com/api/v1/events')
+    .then(function (res) {
+      callback(
+        null,
+        res.data.reduce((acc, event) => {
+          return acc.concat(event.startTime);
+        }, [])
+      );
+    })
+    .catch(function (err) {
+      console.log(err);
+      callback(err, null);
+    });
+};
