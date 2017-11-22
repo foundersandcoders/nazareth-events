4bc3d9ddb141a6aeb6f65aa317a72698d99861ae
mario <mario.saliba98@gmail.com>
2017-11-22 15:18:49 +0200

    fix the bundled frontend #129

--- public/js/filter_listeners.js
+++ public/js/filter_listeners.js
@@ -1,27 +1,31 @@
 /* global URL type:true axios */
 /* eslint no-global-assign: "error" */
 
-document.getElementById('arLang').addEventListener('click', function () {
-  var search = new URL(window.location.href).search;
-  if (search) {
-    window.location.href = '/ar' + search;
-  } else {
-    window.location.href = '/ar';
-  }
-});
+if (document.getElementById('arLang')) {
+  document.getElementById('arLang').addEventListener('click', function() {
+    var search = new URL(window.location.href).search;
+    if (search) {
+      window.location.href = '/ar' + search;
+    } else {
+      window.location.href = '/ar';
+    }
+  });
 
-document.getElementById('enLang').addEventListener('click', function () {
-  var search = new URL(window.location.href).search;
-  if (search) {
-    window.location.href = '/en' + search;
-  } else {
-    window.location.href = '/en';
-  }
-});
+  document.getElementById('enLang').addEventListener('click', function() {
+    var search = new URL(window.location.href).search;
+    if (search) {
+      window.location.href = '/en' + search;
+    } else {
+      window.location.href = '/en';
+    }
+  });
 
-// event listener for category select in header
-document.getElementById('categoryButton').addEventListener('click', function () {
+  // event listener for category select in header
   document
-    .getElementById('categorySelectContainer')
-    .classList.toggle('hide-cal');
-});
+    .getElementById('categoryButton')
+    .addEventListener('click', function() {
+      document
+        .getElementById('categorySelectContainer')
+        .classList.toggle('hide-cal');
+    });
+}
--- public/js/validate_form.js
+++ public/js/validate_form.js
@@ -1,154 +1,163 @@
 /* global XMLHttpRequest axios */
 
-// add a new place
-document.getElementById('addPlaceIcon').addEventListener('click', function (e) {
-  document.getElementById('addPlaceContainer').classList.toggle('hide');
-  document.getElementById('selectAllPlaces').classList.toggle('hide');
-});
+import axios from 'axios';
 
-function handleValidate (messageBox, message, input, childInputs) {
-  window.location.href = input;
-  if (childInputs) {
-    var inputParent = document.querySelector(input).children;
-    inputParent[0].style = 'border: 0.1rem red solid';
-    inputParent[1].style = 'border: 0.1rem red solid';
-  } else {
-    var container = document.querySelector(input);
-    container.style = 'border: 0.1rem red solid';
+if (document.querySelector('.add-event-form')) {
+  // add a new place
+  document
+    .getElementById('addPlaceIcon')
+    .addEventListener('click', function(e) {
+      document.getElementById('addPlaceContainer').classList.toggle('hide');
+      document.getElementById('selectAllPlaces').classList.toggle('hide');
+    });
+
+  function handleValidate(messageBox, message, input, childInputs) {
+    window.location.href = input;
+    if (childInputs) {
+      var inputParent = document.querySelector(input).children;
+      inputParent[0].style = 'border: 0.1rem red solid';
+      inputParent[1].style = 'border: 0.1rem red solid';
+    } else {
+      var container = document.querySelector(input);
+      container.style = 'border: 0.1rem red solid';
+    }
+    document.querySelector(messageBox).innerHTML = message;
   }
-  document.querySelector(messageBox).innerHTML = message;
-}
 
-function validated (input, messageBox, childInputs) {
-  if (childInputs) {
-    var inputParent = document.querySelector(input).children;
-    inputParent[0].style = 'border: 0.1rem gray solid';
-    inputParent[1].style = 'border: 0.1rem gray solid';
-  } else {
-    var container = document.querySelector(input);
-    container.style = 'border: 0.1rem gray solid';
+  function validated(input, messageBox, childInputs) {
+    if (childInputs) {
+      var inputParent = document.querySelector(input).children;
+      inputParent[0].style = 'border: 0.1rem gray solid';
+      inputParent[1].style = 'border: 0.1rem gray solid';
+    } else {
+      var container = document.querySelector(input);
+      container.style = 'border: 0.1rem gray solid';
+    }
+    document.querySelector(messageBox).innerHTML = '';
   }
-  document.querySelector(messageBox).innerHTML = '';
-}
 
-function validatedLanguage (input, messageBox, lang) {
-  window.location.href = '#eventName';
-  document.querySelector('#eventName').children[input].style =
-    'border: 0.1rem red solid';
-  var messageBoxElement = document.querySelector(messageBox);
-  messageBoxElement.innerHTML = 'Should contain ' + lang + ' characters only';
-  messageBoxElement.style = 'margin-top: 0.5rem;';
-}
+  function validatedLanguage(input, messageBox, lang) {
+    window.location.href = '#eventName';
+    document.querySelector('#eventName').children[input].style =
+      'border: 0.1rem red solid';
+    var messageBoxElement = document.querySelector(messageBox);
+    messageBoxElement.innerHTML = 'Should contain ' + lang + ' characters only';
+    messageBoxElement.style = 'margin-top: 0.5rem;';
+  }
 
-// form event listener
-document
-  .querySelector('.add-event-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
+  // form event listener
+  document
+    .querySelector('.add-event-form')
+    .addEventListener('submit', function(event) {
+      event.preventDefault();
 
-    var englishChars = /[a-zA-Z0-9]/;
-    var arabicChars = /[ا-ي0-9]/;
-    var elements = event.target.elements;
-    var data = {
-      nameEn: elements.name_english.value,
-      nameAr: elements.name_arabic.value,
-      descriptionEn: elements.description_en.value,
-      descriptionAr: elements.description_ar.value,
-      categories: elements.categories,
-      placeId: elements.placeId.value,
-      accessibilityOptions: elements.accessibilityOptions.value,
-      startDate: elements.startDate.value,
-      startTime: elements.startTime.value,
-      endTime: elements.endTime.value,
-      cost: elements.cost.value,
-      imageUrl: elements.imageUrl.value,
-      place_name_en: elements.place_name_en.value,
-      place_name_ar: elements.place_name_ar.value
-    };
+      var englishChars = /[a-zA-Z0-9]/;
+      var arabicChars = /[ا-ي0-9]/;
+      var elements = event.target.elements;
+      var data = {
+        nameEn: elements.name_english.value,
+        nameAr: elements.name_arabic.value,
+        descriptionEn: elements.description_en.value,
+        descriptionAr: elements.description_ar.value,
+        categories: elements.categories,
+        placeId: elements.placeId.value,
+        accessibilityOptions: elements.accessibilityOptions.value,
+        startDate: elements.startDate.value,
+        startTime: elements.startTime.value,
+        endTime: elements.endTime.value,
+        cost: elements.cost.value,
+        imageUrl: elements.imageUrl.value,
+        place_name_en: elements.place_name_en.value,
+        place_name_ar: elements.place_name_ar.value
+      };
 
-    if (!data.nameEn && !data.nameAr) {
-      return handleValidate(
-        '.msg-box-input',
-        'Please fill either English or Arabic',
-        '#eventName',
-        true
-      );
-    } else {
-      validated('#eventName', '.msg-box-input', true);
-    }
-
-    if (!data.descriptionEn && !data.descriptionAr) {
-      return handleValidate(
-        '.msg-box-description',
-        'Please fill either English or Arabic',
-        '#eventDescription',
-        true
-      );
-    } else {
-      validated('#eventDescription', '.msg-box-description', true);
-    }
-
-    // check name inputs languages
-    if (data.nameEn && !englishChars.test(data.nameEn)) {
-      return validatedLanguage(0, '.msg-box-input', 'English');
-    } else if (data.nameAr && !arabicChars.test(data.nameAr)) {
-      return validatedLanguage(1, '.msg-box-input', 'Arabic');
-    } else {
-      validated('#eventName', '.msg-box-input', true);
-    }
-
-    if (data.descriptionEn && !englishChars.test(data.descriptionEn)) {
-      return validatedLanguage(0, '.msg-box-description', 'English');
-    } else if (data.descriptionAr && !arabicChars.test(data.descriptionAr)) {
-    } else {
-      validated('#eventDescription', '.msg-box-input', true);
-    }
-    var categoryChecks = 0;
-    var checkedCategories = [];
-    for (var i in data.categories) {
-      if (data.categories[i].checked) {
-        checkedCategories.push(data.categories[i].value);
-        categoryChecks += 1;
-      }
-    }
-
-    if (categoryChecks === 0) {
-      return handleValidate(
-        '.msg-box-categories',
-        'Choose atleast 1 category from the list',
-        '#categoriesBox',
-        false
-      );
-    } else {
-      data.categories = checkedCategories;
-      validated('#categoriesBox', '.msg-box-categories', false);
-    }
-
-    if (!data.placeId) {
-      if (!data.place_name_en && !data.place_name_ar) {
-        handleValidate(
-          '.msg-box-place',
-          'Choose or create a place',
-          '#placeName',
+      if (!data.nameEn && !data.nameAr) {
+        return handleValidate(
+          '.msg-box-input',
+          'Please fill either English or Arabic',
+          '#eventName',
           true
         );
       } else {
-        validated('#placeName', '.msg-box-place', true);
+        validated('#eventName', '.msg-box-input', true);
       }
-    }
 
-    if (data.accessibilityOptions) {
-      var checkedAccessibility = [];
-      for (i in data.accessibilityOptions) {
-        if (data.accessibilityOptions[i].checked) {
-          checkedAccessibility.push(data.accessibilityOptions[i].value);
+      if (!data.descriptionEn && !data.descriptionAr) {
+        return handleValidate(
+          '.msg-box-description',
+          'Please fill either English or Arabic',
+          '#eventDescription',
+          true
+        );
+      } else {
+        validated('#eventDescription', '.msg-box-description', true);
+      }
+
+      // check name inputs languages
+      if (data.nameEn && !englishChars.test(data.nameEn)) {
+        return validatedLanguage(0, '.msg-box-input', 'English');
+      } else if (data.nameAr && !arabicChars.test(data.nameAr)) {
+        return validatedLanguage(1, '.msg-box-input', 'Arabic');
+      } else {
+        validated('#eventName', '.msg-box-input', true);
+      }
+
+      if (data.descriptionEn && !englishChars.test(data.descriptionEn)) {
+        return validatedLanguage(0, '.msg-box-description', 'English');
+      } else if (data.descriptionAr && !arabicChars.test(data.descriptionAr)) {
+      } else {
+        validated('#eventDescription', '.msg-box-input', true);
+      }
+      var categoryChecks = 0;
+      var checkedCategories = [];
+      for (var i in data.categories) {
+        if (data.categories[i].checked) {
+          checkedCategories.push(data.categories[i].value);
+          categoryChecks += 1;
         }
       }
-      data.accessibilityOptions = checkedAccessibility;
-    }
 
-    axios.post('/add-event', data).then(function (res) {
-      console.log(res.request.responseURL);
-      window.location = res.request.responseURL;
+      if (categoryChecks === 0) {
+        return handleValidate(
+          '.msg-box-categories',
+          'Choose atleast 1 category from the list',
+          '#categoriesBox',
+          false
+        );
+      } else {
+        data.categories = checkedCategories;
+        validated('#categoriesBox', '.msg-box-categories', false);
+      }
+
+      if (!data.placeId) {
+        if (!data.place_name_en && !data.place_name_ar) {
+          handleValidate(
+            '.msg-box-place',
+            'Choose or create a place',
+            '#placeName',
+            true
+          );
+        } else {
+          validated('#placeName', '.msg-box-place', true);
+        }
+      }
+
+      if (data.accessibilityOptions) {
+        var checkedAccessibility = [];
+        for (i in data.accessibilityOptions) {
+          if (data.accessibilityOptions[i].checked) {
+            checkedAccessibility.push(data.accessibilityOptions[i].value);
+          }
+        }
+        data.accessibilityOptions = checkedAccessibility;
+      }
+
+      axios
+        .post('/add-event', data)
+        .then(function(res) {
+          console.log('responsasljkdbaihhdgiausdviksajdbkjasbdkha e', res);
+          window.location = res.request.responseURL;
+        })
+        .catch(err => console.log('err', err));
     });
-  });
+}
