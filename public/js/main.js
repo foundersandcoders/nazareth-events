2a5bf6d4c00ca221fefdcdebe608185bb3557ab1
mario <mario.saliba98@gmail.com>
2017-11-25 12:38:58 +0200

    refactor main.js code to es6 #136

--- public/js/main.js
+++ public/js/main.js
@@ -3,27 +3,25 @@ import formatDate from './helpers/date';
 let addDateEventListeners;
 
 if (document.getElementById('list-page-content')) {
-  function dateClickHandler(e) {
-    var date = e.target.dataset.date;
-    window.location.href =
-      new URL(window.location.href).pathname +
-      '?date_from=' +
-      date +
-      '&date_to=' +
-      formatDate(date);
+  const dateClickHandler = e => {
+    const { date } = e.target.dataset;
+    window.location.href = `${
+      new URL(window.location.href).pathname
+    }?date_from=${date}&date_to=
+      ${formatDate(date)}`;
 
     document.getElementById('list-page-content').classList.toggle('hide');
     document.getElementById('show-cal').classList.toggle('hide-cal');
-  }
+  };
 
   // event listener for calendar button in header
-  document.getElementById('calendarIcon').addEventListener('click', function() {
+  document.getElementById('calendarIcon').addEventListener('click', () => {
     document.getElementById('list-page-content').classList.toggle('hide');
     document.getElementById('show-cal').classList.toggle('hide-cal');
   });
 
   addDateEventListeners = () => {
-    document.querySelectorAll('td.day').forEach(function(day) {
+    document.querySelectorAll('td.day').forEach(day => {
       day.removeEventListener('click', dateClickHandler);
       day.addEventListener('click', dateClickHandler);
     });
