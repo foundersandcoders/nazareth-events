727f41c254c652f77d46c27091a17e8eeca943cc
mario <mario.saliba98@gmail.com>
2017-12-11 15:08:00 +0200

    removes code that relies on the files that were removed #143

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,7 @@ const router = require('express').Router();
 
 const homePage = require('./home.js');
 const detailsPage = require('./event_details.js');
-const addEventForm = require('./render_add_form.js');
-const addEvent = require('./add_event.js');
-const handleOauth = require('./oauth.js');
-const addPlace = require('./middleware/post_place.js');
-const getAllPlaces = require('./middleware/get_places.js');
-const authenticate = require('./middleware/authenticate');
 
-router.get('/add-event', authenticate, getAllPlaces, addEventForm);
-router.post('/add-event', addPlace, addEvent);
-router.get('/token', handleOauth);
 router.get('/:lang?', homePage);
 router.get('/:lang/events/:id', detailsPage);
 
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const compression = require('compression');
 const router = require('./routes/index.js');
 const dateFormat = require('dateformat');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 const server = express();
 
@@ -14,9 +12,6 @@ server.set('view engine', 'hbs');
 server.set('views', path.join(__dirname, './', 'views'));
 
 server.use(express.static(path.join(__dirname, './', 'public')));
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
-server.use(cookieParser());
 server.use(compression());
 
 server.use(router);
