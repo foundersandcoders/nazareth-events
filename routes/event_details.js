fd31fe1508aaa482beeed19712468b3be5bd6155
mario <mario.saliba98@gmail.com>
2017-12-18 15:19:36 +0200

    fix google maps in cae address is not found #158

--- routes/event_details.js
+++ routes/event_details.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 
 module.exports = async (req, res) => {
   const url = `${process.env.PRODUCTION_API}/events/${req.params.id}`;
+  function getCoords(address) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const geocodeRes = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${
+            address
+          }&region=il&key=${process.env.GEOCODE_KEY}`
+        );
+        resolve(geocodeRes);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 
   try {
     const specificEventResponse = await axios.get(url);
@@ -12,16 +26,35 @@ module.exports = async (req, res) => {
     if (event.place.location) {
       lat = event.place.location[0];
       lng = event.place.location[1];
-    } else {
+    } else if (event.place[req.params.lang].address.trim()) {
       let address = event.place[req.params.lang].address;
-      const geocode = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${
-          address
-        }&region=il&key=${process.env.GEOCODE_KEY}`
-      );
-      const { location } = geocode.data.results[0].geometry;
-      lng = location.lng;
-      lat = location.lat;
+      const geocode = await getCoords(address);
+      console.log('here', geocode);
+      if (geocode.data.status === 'OK') {
+        const { location } = geocode.data.results[0].geometry;
+        lat = location.lat;
+        lng = location.lng;
+      } else {
+        // Nazareth coords
+        lat = 32.699635;
+        lng = 35.303546;
+      }
+    } else if (event.place.en.address.trim()) {
+      let address = event.place.en.address;
+      const geocode = await getCoords(address);
+      if (geocode.data.status === 'OK') {
+        const { location } = geocode.data.results[0].geometry;
+        lat = location.lat;
+        lng = location.lng;
+      } else {
+        // Nazareth coords
+        lat = 32.699635;
+        lng = 35.303546;
+      }
+    } else {
+      // Nazareth coords
+      lat = 32.699635;
+      lng = 35.303546;
     }
 
     res.render('event_details', {
@@ -40,6 +73,7 @@ module.exports = async (req, res) => {
         : ''
     });
   } catch (err) {
+    console.log('error', err);
     res.render('error', {
       errorMessage: 'Sorry something went wrong please try again'
     });
